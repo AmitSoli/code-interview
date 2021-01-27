2147c5139f6c45fa54738e6f6b1d10062844f7bf
AmitSoli <soliamit@gmail.com>
2021-01-27 14:02:57 +0200

    add fixes and post

--- src/controllers/appointment.js
+++ src/controllers/appointment.js
@@ -28,22 +28,42 @@ const isProviderAvailable = (dates, reqDate) => {
     return isAvailable
 }
 
+
+const specialtyIncluded = (specialties, wantedSpecialty) => {
+    
+    const isIncluded = specialties.find((specialty) => {
+        return wantedSpecialty.toLowerCase() == specialty.toLowerCase()
+    })
+    return isIncluded
+}
+
+const compareByScore = (a, b) => {
+    if (a.score > b.score){
+        return -1
+    }
+    return 1
+}
+
+
 exports.getAppointments = async (req, res) => { 
     try {
         //const appointmentRequest = new AppointmentRequest(req.params)
-        console.log(req.query)
+        //console.log(req.query)
         const minScore = req.query.minScore
         const specialty = req.query.specialty
         const requestedDate = req.query.date
+        
+
         console.log(minScore,specialty,requestedDate)
         const providers = loadProviders()
-        const relevantProviders = providers.filter((provider) => {
-            console.log(provider)
+        let relevantProviders = providers.filter((provider) => {
+            //console.log(provider)
             return provider.score >= minScore &&
-                    provider.specialties.includes(specialty) &&
+                    specialtyIncluded(provider.specialties,specialty) &&
                     isProviderAvailable(provider.availableDates,requestedDate)            
         })
-        res.send(relevantProviders)
+        relevantProviders.sort(compareByScore)
+        res.send(relevantProviders.map((provider) => provider.name))
     } catch (e) {
         console.log("failed sending providers")
         res.status(500).send()
@@ -52,5 +72,28 @@ exports.getAppointments = async (req, res) => {
 
 
 
-exports.postAppointment = async (req, res) => { }
+exports.postAppointment = async (req, res) => {
+    try {
+        console.log(req.body)
+        const name = req.body.name
+        const date = req.body.date
+        const providers = loadProviders()
+        const chosenProvider = providers.find((provider) => {
+            return name === provider.name
+        })
+
+        if (!chosenProvider){
+            console.log('Provider was not found')
+            return res.status(400).send();
+        }
+        if (!isProviderAvailable(chosenProvider.availableDates,date)){
+            console.log('Provider is not available')
+            return res.status(400).send();
+        }
+        return res.status(200).send()
+
+    } catch (e) {
+        
+    }
+}
 
